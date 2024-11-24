879326187125cb17ad63d8ffddd2188fcc7b7f3f
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-24 17:37:25 +0200

    feat: add get all tours route

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import express from 'express';
+import fs from 'fs';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({message:'Hi from the server side..'});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'SUCCESS',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
-
 app.listen(port, () => {
   console.log(`Application started running on port ${port}`);
 });
