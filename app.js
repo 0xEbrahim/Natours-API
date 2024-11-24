93e18ff7eace146a751d3f91e144b24ce7ac818e
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-24 18:00:30 +0200

    feat: add create a new tour route

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-
 import express from 'express';
 import fs from 'fs';
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: 'SUCCESS',
+    status: 'Success',
     results: tours.length,
     data: {
       tours,
     },
   });
 });
+
+app.post('/api/v1/tours', async (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          newTour,
+        },
+      });
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Application started running on port ${port}`);
 });
