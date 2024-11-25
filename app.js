f42305145057cd1a2d6abc13d5b99b7996b40ff8
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-25 10:10:22 +0200

    feat: add user routes and controller

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-
 import express from 'express';
 import morgan from 'morgan';
-
 import tourRouter from './routes/tourRoutes.js';
+import userRouter from './routes/userRoutes.js';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan('dev'));
 
-
-
 app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Application started running on port ${port}`);
