d5114bf5920a61ba9d098cd67b31f3f04f3222c3
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-26 13:01:00 +0200

    feat: adding Tour data model

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ import app from './app.js';
 
 mongoose
   .connect(process.env.DATABASE_URI)
-  .then(() => {
-    console.log('Connected to database successfully..');
-  })
+  .then(() => console.log('Connected to database successfully..'))
   .catch(err => {
     throw err;
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Application started running on port ${port}`);
-});
+app.listen(port, () =>
+  console.log(`Application started running on port ${port}`)
+);
