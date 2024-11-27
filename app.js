5599a10de7a59934e8fe0cbb4e6884a5f014504b
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-27 23:25:26 +0200

    feat: add global error handling middlware

--- app.js
+++ app.js
@@ -21,5 +21,12 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', handleUnhandledRoutes);
-
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 export default app;
