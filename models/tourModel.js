d5036e4a633ebef552a58552507ee469f9f419b5
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-27 22:29:14 +0200

    chore: add mongodb validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,7 +7,15 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
-      trim: true
+      trim: true,
+      maxlength: [
+        40,
+        'A tour name must have less than or equal to 40 characters'
+      ],
+      minlength: [
+        10,
+        'A tour name must have more than or equal to 10 characters'
+      ]
     },
     slug: {
       type: String
@@ -22,11 +30,17 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty should be from [easy , medium , difficult]'
+      }
     },
     ratingAverage: {
       type: Number,
-      default: 0.0
+      default: 0.0,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be less than 5.0']
     },
     ratingQuantity: {
       type: Number,
