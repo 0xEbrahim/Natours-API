6453946e62aed2ad8299b080c8c3af3268aff382
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-27 22:22:44 +0200

    chore: adding some mongodb middlwares

--- models/tourModel.js
+++ models/tourModel.js
@@ -62,7 +62,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [{ type: Date }]
+    startDates: [{ type: Date }],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: {
@@ -78,8 +82,31 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+// Doc middlware
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Query middlware that works on the query object and modify it before execution
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took: ${Date.now() - this.start} ms`);
+  next();
+});
+
+// Aggregation middlware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({
+    $match: {
+      secretTour: { $ne: true }
+    }
+  });
   next();
 });
 
