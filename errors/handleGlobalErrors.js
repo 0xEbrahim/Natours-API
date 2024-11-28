583a0bdcd1caf39e5bdd3ccc7a78001e31f35750
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-28 08:32:47 +0200

    refactor: sparate error during production and development

--- errors/handleGlobalErrors.js
+++ errors/handleGlobalErrors.js
@@ -1,9 +1,33 @@
+const sendDevErrors = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack
+  });
+};
+
+const sendProdErrors = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
+};
 const handleGlobalErrors = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+
+  if (process.env.NODE_ENV == 'development') {
+    sendDevErrors(err, res);
+  } else if (process.env.NODE_ENV == 'production') {
+    sendProdErrors(err, res);
+  }
 };
 export { handleGlobalErrors };
