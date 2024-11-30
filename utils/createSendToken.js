74772717cdd4b0fbdb20b3c8de3b7f3a513f532c
Ebrahim <elsayedebrahim603@gmail.com>
2024-11-30 20:18:56 +0200

    feat: add JWT into cookies

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -2,7 +2,17 @@ import { signToken } from './JWT.js';
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-   res.status(statusCode).json({
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
+  res.status(statusCode).json({
     status: 'success',
     token,
     data: { user }
